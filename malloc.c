246a6619544dbcc77df2aa6a35496f5289ce1a4f
Manthan Thakar <manthant15@gmail.com>
2017-10-07 17:14:20 -0400

    create find_order method
    Find the order of the requested size in terms of the free list.
    
    Also, format the code in clang format

--- malloc.c
+++ malloc.c
@@ -4,7 +4,9 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <math.h>
 
+// FIXME: Fix the internal fragmentation problem pointed out by the professor
 
 #define ERROR(e) \
   {              \
@@ -13,14 +15,16 @@
   }
 
 // Enum to assign a status to the memory blocks
-typedef enum block_status_t {USED, FREE} BlockStatus;
+typedef enum block_status_t { USED, FREE } BlockStatus;
+
+const int MIN_ORDER = 3, MAX_ORDER = 12;
 
 // Memory Block to store metdata about the allocated memory blocks
 typedef struct block_t {
-	size_t size;
-	struct block_t* next;
-	struct block_t* previous;
-	BlockStatus status;
+  size_t size;
+  struct block_t *next;
+  struct block_t *previous;
+  BlockStatus status;
 } Block;
 
 // Location of break marker in the heap
@@ -33,28 +37,49 @@ static void *heap = NULL;
  * blocks[0] stores blocks having size 8 bytes
  * and blocks[9] stores blocks having size 4096 bytes
  */
-static Block* blocks[10];
+static Block *blocks[10];
 
 /**
  * Initialize the malloc library by extending heap size
  */
 void init() {
-	// Increment the data segment size by page size
-	if((heap_break = sbrk(sysconf(_SC_PAGESIZE))) == (void *) -1) ERROR("sbrk");
-	blocks[9] = (Block *) heap_break;
-	blocks[9]->size = 4096;
-	blocks[9]->next = NULL;
-	blocks[9]->previous = NULL;
-	blocks[9]->status = FREE;
+  // Increment the data segment size by page size
+  if ((heap = sbrk(sysconf(_SC_PAGESIZE))) == (void *)-1) ERROR("sbrk");
+  blocks[9] = (Block *)heap;
+  blocks[9]->size = 4096;
+  blocks[9]->next = NULL;
+  blocks[9]->previous = NULL;
+  blocks[9]->status = FREE;
 }
 
-int my_malloc(size_t size) {
-	// Initialize if heap_break doesn't point to anything
-	if(!heap) init();
+void *my_malloc(size_t size) {
+  // Initialize if heap_break doesn't point to anything
+  if (!heap) init();
+
+  // Heap space already allocated
+  size_t totalSize = size + sizeof(Block);
+  printf("Order: %d\n", find_order(totalSize));
+  // Find the order for the requested size
+  // printf("Size asked for is %zu\n", totalSize );
+}
+
+int find_order(size_t size) {
+  printf("Finding order for %zu bytes\n", size);
+  int order = MAX_ORDER;
+  while(order >= MIN_ORDER) {
+    if(size <= pow(2, order) && size > pow(2, order - 1)) {
+      break;
+    }
+    order--;
+  }
+  // The order index in the blocks array starts from 0,
+  // so we subtract the smallest order out of the actual
+  // order so that we can search the free blocks from the
+  // correct list
+  return order - MIN_ORDER;
 }
 
 int main() {
-	init();
-	printf("Initialized Heap!\n");
-	return 0;
+  my_malloc(2048);
+  return 0;
 }
