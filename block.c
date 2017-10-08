5cfcb21a19c057fff96bab98320b299ed74d2240
Manthan Thakar <manthant15@gmail.com>
2017-10-08 04:00:02 -0400

    Format code in clang format

--- block.c
+++ block.c
@@ -1,45 +1,46 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "block.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 Block* new_block(void* addr) {
-	Block* b = (Block *) addr;
-	b->startAddr = addr;
-	b->size = 0;
-	b->next = NULL;
-	b->previous = NULL;
-	b->status = FREE;
-	return b;
+  Block* b = (Block*)addr;
+  b->startAddr = addr;
+  b->size = 0;
+  b->next = NULL;
+  b->previous = NULL;
+  b->status = FREE;
+  return b;
 }
 
-void* alloc_block(Block *b, size_t totalSize) {
-	Block *freeblock = b;
-    freeblock->size = totalSize;
-    freeblock->status = USED;
-    b = freeblock->next;
-    if(b != NULL) b->previous = NULL;
-    freeblock->next = NULL;
-    return freeblock->startAddr + sizeof(Block);
+Block* mark_block(Block* b, size_t totalSize) {
+  Block* freeblock = b->startAddr;
+  freeblock->size = totalSize;
+  freeblock->status = USED;
+  return freeblock;
 }
 
 void print_block(Block* b, int level) {
-	printf("----- Block Information (%d)-----\n", level);
-	printf("\taddr: %p\n", b->startAddr);
-	printf("\tsize: %zu\n", b->size);
-	printf("\tstatus: %d\n", b->status);
-	printf("\tNext: %p\n", b->next);
-	printf("\tPrevious: %p\n", b->previous);
-	printf("------------------------------\n");
+  if (b != NULL) {
+    printf("----- Block Information (%d)-----\n", level);
+    printf("\taddr: %p\n", b->startAddr);
+    printf("\tsize: %zu\n", b->size);
+    printf("\tstatus: %d\n", b->status);
+    printf("\tNext: %p\n", b->next);
+    printf("\tPrevious: %p\n", b->previous);
+    printf("------------------------------\n");
+  }
 }
 
-void print_blocklist(Block **blocks, int length) {
-	int i;
-	for(i = 0; i < length; i++) {
-		printf("Level %d\n", i);
-		Block *b = blocks[i];
-		while(b != NULL) {
-			print_block(b, i);
-			b = b->next;
-		}
-	}
+void print_blocklist(Block** blocks, int length) {
+  assert(length > 0);
+  int i;
+  for (i = 0; i < length; i++) {
+    printf("Level %d\n", i);
+    Block* b = blocks[i];
+    while (b != NULL) {
+      print_block(b, i);
+      b = b->next;
+    }
+  }
 }
--- block.h
+++ block.h
@@ -6,11 +6,11 @@ typedef enum block_status_t { USED, FREE } BlockStatus;
 
 // Memory Block to store metdata about the allocated memory blocks
 typedef struct block_t {
-  void *startAddr; // Starting address of the block
-  size_t size; // Size occupied at this block
-  struct block_t *next; // Next block
-  struct block_t *previous; // Previous Block
-  BlockStatus status; // If the block is occupied or free
+  void *startAddr;           // Starting address of the block
+  size_t size;               // Size occupied at this block
+  struct block_t *next;      // Next block
+  struct block_t *previous;  // Previous Block
+  BlockStatus status;        // If the block is occupied or free
 } Block;
 
 /**
@@ -25,12 +25,12 @@ typedef struct block_t {
  * `previous`: NULL
  * `status`: FREE
  */
-Block* new_block(void*);
+Block *new_block(void *);
 
-void* alloc_block(Block*, size_t);
+Block *mark_block(Block *, size_t);
 
-void print_block(Block*, int);
+void print_block(Block *, int);
 
-void print_blocklist(Block**, int);
+void print_blocklist(Block **, int);
 
 #endif
