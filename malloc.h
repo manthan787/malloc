0e08a94d7346e929818c69f6d5f274d49c28701e
Manthan Thakar <manthant15@gmail.com>
2017-10-15 17:57:03 -0400

    Add mallinfo in malloc.h

--- malloc.h
+++ malloc.h
@@ -2,7 +2,27 @@
 #define MALLOC_H
 
 #include <stdlib.h> // For size_t
+#include "config.h"
+#include "block.h"
 
-void* my_malloc(size_t);
+void* malloc(size_t);
 
+/** print malloc's stats for each arena */
+void malloc_stats();
+
+struct mallinfo {
+   int numArenas; /* Total number of arenas */
+   int allocreq;  /* Total number of allocation requests */
+   int freereq;   /* Total number of free requests */
+   int hblks;     /* Number of mmapped regions */
+   int hblkhd;    /* Space allocated in mmapped regions (bytes) */
+   int ordblks;   /* Number of free chunks */
+   int uordblks;  /* Total allocated space (bytes) */
+   int fordblks;  /* Total free space (bytes) */
+};
+
+struct mallinfo stats;
+static __thread void *heap = NULL;
+static __thread Block *blocks[MAX_INDEX + 1];
+static __thread pthread_mutex_t mutex;
 #endif
